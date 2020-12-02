e56130efcdcb96c48b6cea5ed86aa98413cc18b0
SuryaRSoman <suryasurabhi5@gmail.com>
2020-12-02 05:13:45 -0800

    background image added

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product App</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="bg">
     <div class="container">
         <div class="row">
         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
